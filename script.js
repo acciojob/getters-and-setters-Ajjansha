57c1e9b8b6ff5d483661390cd601af9a7ba23056
Ajjansha <141652880+Ajjansha@users.noreply.github.com>
2026-01-17 00:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	Constructor(name, age){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this.name;
+	}
+	set name(newName){
+		this.name=newName;
+	}
+	set age(newAge){
+		this.age=newAge;
+	}
+	get age(){
+		return this.age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+	return(this.name + " is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+	return(this.name + " is teaching")
+}}
 
 // Do not change the code below this line
 window.Person = Person;
